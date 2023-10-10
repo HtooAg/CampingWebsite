9b5689023cb59ac020f8e2a395fc8e2fc69c6a60
HtooAg <htoow243@gmail.com>
2023-10-10 13:07:23 +0630

    Added all features

--- js/HamToggle.js
+++
@@ -1,7 +0,0 @@
-const navbarLinks = document.querySelector(".navbar .nav-items");
-
-const navbarToggle = document.querySelector(".navbar .navbar-toggle");
-const toggleBar = navbarToggle.addEventListener("click", () => {
-	navbarLinks.classList.toggle("show");
-	navbarToggle.classList.toggle("active");
-});
---
+++ js/index.js
@@ -0,0 +1,82 @@
+// HamburgerToggle
+const navbarLinks = document.querySelector(".navbar .nav-items");
+
+const navbarToggle = document.querySelector(".navbar .navbar-toggle");
+const toggleBar = navbarToggle.addEventListener("click", () => {
+	navbarLinks.classList.toggle("show");
+	navbarToggle.classList.toggle("active");
+});
+
+// Slider
+const swiper = new Swiper(".slider .swiper", {
+	pagination: {
+		el: ".slider .swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".slider .swiper-button-next",
+		prevEl: ".slider .swiper-button-prev",
+	},
+	loop: true,
+	autoplay: {
+		delay: 2000,
+	},
+});
+
+// Product Slider
+const swipers = new Swiper("#swiper", {
+	spaceBetween: 30,
+	loop: true,
+	effect: "slide",
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	autoplay: false,
+	breakpoints: {
+		1280: {
+			slidesPerView: 4,
+		},
+		989: {
+			slidesPerView: 3,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		500: {
+			slidesPerView: 1,
+		},
+	},
+});
+
+// Item Button
+const updateCount = (count, countDisplay) => {
+	countDisplay.textContent = count;
+};
+
+for (let i = 1; i <= 10; i++) {
+	const decrementButton = document.querySelector(`#decrement${i}`);
+	const incrementButton = document.querySelector(`#increment${i}`);
+	const countDisplay = document.querySelector(`#count${i}`);
+
+	if (decrementButton && incrementButton && countDisplay) {
+		let count = 0;
+
+		decrementButton.addEventListener("click", () => {
+			if (count > 0) {
+				count -= 1;
+				updateCount(count, countDisplay);
+			}
+		});
+
+		incrementButton.addEventListener("click", () => {
+			count += 1;
+			updateCount(count, countDisplay);
+		});
+
+		updateCount(count, countDisplay);
+	}
+}
+
+// AOS
+AOS.init();
