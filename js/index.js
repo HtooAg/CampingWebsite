2043830163079abbd26413e89810367851d50b46
HtooAg <htoow243@gmail.com>
2023-10-12 21:34:02 +0630

    Added Pagination

--- js/index.js
+++ js/index.js
@@ -54,7 +54,7 @@ const updateCount = (count, countDisplay) => {
 	countDisplay.textContent = count;
 };
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 50; i++) {
 	const decrementButton = document.querySelector(`#decrement${i}`);
 	const incrementButton = document.querySelector(`#increment${i}`);
 	const countDisplay = document.querySelector(`#count${i}`);
@@ -80,3 +80,70 @@ for (let i = 1; i <= 10; i++) {
 
 // AOS
 AOS.init();
+
+// Pagination
+
+const itemsPerPage = 4;
+const $cards = $(".card");
+const $pagination = $(".pagination");
+const pageCount = Math.ceil($cards.length / itemsPerPage);
+let currentPage =
+	localStorage.getItem("currentPage") === null
+		? 1
+		: parseInt(localStorage.getItem("currentPage"));
+
+const showPageWithDelay = page => {
+	currentPage = page;
+	localStorage.setItem("currentPage", page);
+	const startIndex = (page - 1) * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+
+	$cards.hide();
+
+	setTimeout(() => {
+		$cards.slice(startIndex, endIndex).show();
+		refreshAOSWithDelay();
+	}, 500);
+};
+
+const refreshAOSWithDelay = () => {
+	setTimeout(() => {
+		AOS.refresh();
+	}, 500);
+};
+
+$pagination.on("click", ".page-link", e => {
+	e.preventDefault();
+	const pageText = $(e.target).text();
+	const page = getPageFromText(pageText);
+	showPageWithDelay(page);
+});
+
+$pagination.on("click", ".prev-page", e => {
+	e.preventDefault();
+	if (currentPage > 1) {
+		showPageWithDelay(currentPage - 1);
+	}
+});
+
+$pagination.on("click", ".next-page", e => {
+	e.preventDefault();
+	if (currentPage < pageCount) {
+		showPageWithDelay(currentPage + 1);
+	}
+});
+
+showPageWithDelay(1);
+
+const getPageFromText = pageText => {
+	switch (pageText) {
+		case "Tents":
+			return 1;
+		case "Camping Furniture":
+			return 2;
+		case "Cookwares":
+			return 3;
+		default:
+			return 1;
+	}
+};
