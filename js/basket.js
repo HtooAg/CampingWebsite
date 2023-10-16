73a61ebff0310ecd205eaed9f7563646756c28a4
HtooAg <htoow243@gmail.com>
2023-10-16 23:44:45 +0630

    Added basket

---
+++ js/basket.js
@@ -0,0 +1,94 @@
+// AddtoCart
+document.addEventListener("DOMContentLoaded", function () {
+	const cart = JSON.parse(localStorage.getItem("cart")) || [];
+	const updateCount = (count, countDisplay) => {
+		countDisplay.textContent = count;
+	};
+
+	const addToCartButtons = document.querySelectorAll(".addtocart");
+
+	addToCartButtons.forEach((addToCartButton, index) => {
+		const decrementButton = document.getElementById(
+			`decrement${index + 1}`
+		);
+		const incrementButton = document.getElementById(
+			`increment${index + 1}`
+		);
+		const countDisplay = document.getElementById(`count${index + 1}`);
+
+		let count = 0;
+
+		decrementButton.addEventListener("click", () => {
+			if (count > 0) {
+				count -= 1;
+				updateCount(count, countDisplay);
+			}
+		});
+
+		incrementButton.addEventListener("click", () => {
+			count += 1;
+			updateCount(count, countDisplay);
+		});
+
+		addToCartButton.addEventListener("click", () => {
+			const productName =
+				addToCartButton.getAttribute("data-product-name");
+			const productPrice = parseFloat(
+				addToCartButton.getAttribute("data-product-price")
+			);
+			const quantity = count;
+
+			const productData = {
+				name: productName,
+				price: productPrice,
+				quantity: quantity
+			};
+
+			cart.push(productData);
+			localStorage.setItem("cart", JSON.stringify(cart));
+
+			alert(`Added "${productName}" to the cart.`);
+			window.location.href = "Basket.html";
+		});
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	const cart = JSON.parse(localStorage.getItem("cart")) || [];
+	const cartItemsContainer = document.getElementById("cart-items");
+	let totalAmount = 0;
+
+	cart.forEach(function (productData) {
+		const row = document.createElement("tr");
+		row.innerHTML = `
+            <td>${productData.name}</td>
+            <td>$${
+				productData.price ? productData.price.toFixed(2) : "N/A"
+			}</td>
+            <td>${productData.quantity || "N/A"}</td>
+            <td>$${
+				productData.price && productData.quantity
+					? (productData.price * productData.quantity).toFixed(2)
+					: "N/A"
+			}</td>
+        `;
+		cartItemsContainer.appendChild(row);
+
+		if (productData.price && productData.quantity) {
+			totalAmount += productData.price * productData.quantity;
+		}
+	});
+
+	const totalRow = document.createElement("tr");
+	totalRow.innerHTML = `
+        <td colspan="3">Total:</td>
+        <td>$${totalAmount.toFixed(2)}</td>
+    `;
+	cartItemsContainer.appendChild(totalRow);
+});
+
+const resetCart = () => {
+	const cart = [];
+	localStorage.setItem("cart", JSON.stringify(cart));
+	location.reload();
+};
--- js/index.js
+++ js/index.js
@@ -11,16 +11,16 @@ const toggleBar = navbarToggle.addEventListener("click", () => {
 const swiper = new Swiper(".slider .swiper", {
 	pagination: {
 		el: ".slider .swiper-pagination",
-		clickable: true,
+		clickable: true
 	},
 	navigation: {
 		nextEl: ".slider .swiper-button-next",
-		prevEl: ".slider .swiper-button-prev",
+		prevEl: ".slider .swiper-button-prev"
 	},
 	loop: true,
 	autoplay: {
-		delay: 2500,
-	},
+		delay: 2500
+	}
 });
 
 // Product Slider
@@ -31,57 +31,28 @@ const initializeSwiper = selector => {
 		effect: "slide",
 		navigation: {
 			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
+			prevEl: ".swiper-button-prev"
 		},
 		autoplay: false,
 		breakpoints: {
 			1280: {
-				slidesPerView: 4,
+				slidesPerView: 4
 			},
 			989: {
-				slidesPerView: 3,
+				slidesPerView: 3
 			},
 			768: {
-				slidesPerView: 2,
+				slidesPerView: 2
 			},
 			500: {
-				slidesPerView: 1,
-			},
-		},
+				slidesPerView: 1
+			}
+		}
 	});
 };
 
 const swipers = initializeSwiper("#swiper");
 const swiper1 = initializeSwiper("#swiper1");
-
-// Item Button
-const updateCount = (count, countDisplay) => {
-	countDisplay.textContent = count;
-};
-
-for (let i = 1; i <= 100; i++) {
-	const decrementButton = document.querySelector(`#decrement${i}`);
-	const incrementButton = document.querySelector(`#increment${i}`);
-	const countDisplay = document.querySelector(`#count${i}`);
-
-	if (decrementButton && incrementButton && countDisplay) {
-		let count = 0;
-
-		decrementButton.addEventListener("click", () => {
-			if (count > 0) {
-				count -= 1;
-				updateCount(count, countDisplay);
-			}
-		});
-
-		incrementButton.addEventListener("click", () => {
-			count += 1;
-			updateCount(count, countDisplay);
-		});
-
-		updateCount(count, countDisplay);
-	}
-}
 
 // AOS
 AOS.init();
