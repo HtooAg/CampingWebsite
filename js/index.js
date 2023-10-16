6b3f740daa4ebb0e8d399b66783dc495037eb937
HtooAg <htoow243@gmail.com>
2023-10-16 12:57:58 +0630

    Added another pages

--- js/index.js
+++ js/index.js
@@ -24,37 +24,42 @@ const swiper = new Swiper(".slider .swiper", {
 });
 
 // Product Slider
-const swipers = new Swiper("#swiper", {
-	spaceBetween: 30,
-	loop: true,
-	effect: "slide",
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	autoplay: false,
-	breakpoints: {
-		1280: {
-			slidesPerView: 4,
+const initializeSwiper = selector => {
+	return new Swiper(selector, {
+		spaceBetween: 30,
+		loop: true,
+		effect: "slide",
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
 		},
-		989: {
-			slidesPerView: 3,
+		autoplay: false,
+		breakpoints: {
+			1280: {
+				slidesPerView: 4,
+			},
+			989: {
+				slidesPerView: 3,
+			},
+			768: {
+				slidesPerView: 2,
+			},
+			500: {
+				slidesPerView: 1,
+			},
 		},
-		768: {
-			slidesPerView: 2,
-		},
-		500: {
-			slidesPerView: 1,
-		},
-	},
-});
+	});
+};
+
+const swipers = initializeSwiper("#swiper");
+const swiper1 = initializeSwiper("#swiper1");
 
 // Item Button
 const updateCount = (count, countDisplay) => {
 	countDisplay.textContent = count;
 };
 
-for (let i = 1; i <= 50; i++) {
+for (let i = 1; i <= 100; i++) {
 	const decrementButton = document.querySelector(`#decrement${i}`);
 	const incrementButton = document.querySelector(`#increment${i}`);
 	const countDisplay = document.querySelector(`#count${i}`);
@@ -80,75 +85,3 @@ for (let i = 1; i <= 50; i++) {
 
 // AOS
 AOS.init();
-
-// Pagination
-const itemsPerPage = 4;
-const $cards = $(".card");
-const $pagination = $(".pagination");
-const pageCount = Math.ceil($cards.length / itemsPerPage);
-
-let currentPage =
-	localStorage.getItem("currentPage") === null
-		? 1
-		: parseInt(localStorage.getItem("currentPage"));
-
-const setActivePage = () => {
-	$pagination.find(".page-link").removeClass("active");
-	$pagination.find(`.page-link:eq(${currentPage - 1})`).addClass("active");
-};
-
-const showPageWithDelay = page => {
-	currentPage = page;
-	localStorage.setItem("currentPage", page);
-	const startIndex = (page - 1) * itemsPerPage;
-	const endIndex = startIndex + itemsPerPage;
-
-	setActivePage();
-
-	$cards.hide();
-	setTimeout(() => {
-		$cards.slice(startIndex, endIndex).show();
-		refreshAOSWithDelay();
-	}, 500);
-};
-
-const refreshAOSWithDelay = () => {
-	setTimeout(() => {
-		AOS.refresh();
-	}, 500);
-};
-
-$pagination.on("click", ".page-link", e => {
-	e.preventDefault();
-	const pageText = $(e.target).text();
-	showPageWithDelay(getPageFromText(pageText));
-});
-
-$pagination.on("click", ".prev-page", e => {
-	e.preventDefault();
-	if (currentPage > 1) {
-		showPageWithDelay(currentPage - 1);
-	}
-});
-
-$pagination.on("click", ".next-page", e => {
-	e.preventDefault();
-	if (currentPage < pageCount) {
-		showPageWithDelay(currentPage + 1);
-	}
-});
-
-showPageWithDelay(currentPage);
-
-const getPageFromText = pageText => {
-	switch (pageText) {
-		case "Tents":
-			return 1;
-		case "Camping Furniture":
-			return 2;
-		case "Cookwares":
-			return 3;
-		default:
-			return 1;
-	}
-};
