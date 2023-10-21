5d9be1b084961927c1c273b7af66ad0bd0cbe25b
HtooAg <htoow243@gmail.com>
2023-10-21 13:49:07 +0630

    Added all features

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 // Google Map
 document.addEventListener("DOMContentLoaded", () => {
 	const googleMapsURL =
-		"https://maps.google.com/maps?width=600&height=400&hl=en&q=University%20of%20Oxford&t=&z=14&ie=UTF8&iwloc=B&output=embed";
+		"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15545572.922165938!2d77.00259756383042!3d17.964500142168134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x305652a7714e2907%3A0xba7b0ee41c622b11!2sMyanmar%20(Burma)!5e0!3m2!1sen!2smm!4v1697870797647!5m2!1sen!2smm";
 
 	const iframe = document.getElementById("gmap_iframe");
 
